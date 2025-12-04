83cf6db941eece7a3990214d948a46fb08b8a683
Kathryn <137423893+kathrynpotten@users.noreply.github.com>
2025-12-04 13:04:00 +0000

    4-2 Printing Department

--- scripts/04_Printing_Department.py
+++ scripts/04_Printing_Department.py
@@ -2,7 +2,6 @@
 
 import itertools
 
-# Part 1
 
 test_data = """..@@.@@@@.
 @@@.@.@.@@
@@ -29,13 +28,15 @@ x.x.@@@.x."""
 
 class PaperMap:
 
-    def __init__(self, map, debug=False):
+    def __init__(self, map, debug=False, debug_fine=False):
         self.map = map
         self.rows = self.map.split("\n")
+        self.grid = self.to_grid()
         self.m = len(self.rows)
         self.n = len(self.rows[0])
         self.size = (self.m, self.n)
         self.debug = debug
+        self.debug_fine = debug_fine
 
     def __str__(self):
         return "\n".join(row for row in self.rows)
@@ -50,32 +51,53 @@ class PaperMap:
             if tup != (i, j) and 0 <= tup[0] < self.m and 0 <= tup[1] < self.n
         )
 
-    def access_search(self):
-        if self.debug:
-            grid = self.to_grid()
+    def access_search(self, removing=False):
+        if removing:
+            adjacent_roll = ["@"]
+        else:
+            adjacent_roll = ["@", "x"]
         accessible = 0
         for i in range(self.m):
             for j in range(self.n):
                 inaccesible = False
                 adjacent = 0
-                if self.rows[i][j] == "@":
+                if self.grid[i][j] == "@":
                     for x, y in self.adjacent_spaces(i, j):
-                        if self.rows[x][y] == "@":
+                        if self.grid[x][y] in adjacent_roll:
                             adjacent += 1
                             if adjacent >= 4:
                                 inaccesible = True
                                 break
                     if not inaccesible:
-                        if self.debug:
-                            grid[i][j] = "x"
+                        self.grid[i][j] = "x"
                         accessible += 1
+                        if self.debug_fine:
+                            grid_repr = []
+                            for row in self.grid:
+                                grid_repr.append("".join(x for x in row))
+                            print("\n".join(row for row in grid_repr))
+                            print("\n")
         if self.debug:
             grid_repr = []
-            for row in grid:
+            for row in self.grid:
                 grid_repr.append("".join(x for x in row))
             print("\n".join(row for row in grid_repr))
+            print("\n")
         return accessible
 
+    def access_with_removal(self):
+        accessible = 0
+        removed = 1
+        while removed > 0:
+            removed = self.access_search(removing=True)
+            accessible += removed
+        return accessible
+
+    def reset_grid(self):
+        self.grid = self.to_grid()
+
+
+# Part 1
 
 test_map = PaperMap(test_data, debug=True)
 
@@ -86,3 +108,12 @@ with open("../input_data/04_Printing_Department.txt", "r", encoding="utf-8") as 
 
 answer_map = PaperMap(input_data)
 print(answer_map.access_search())
+
+# Part 2
+
+
+test_map.reset_grid()
+assert test_map.access_with_removal() == 43
+
+answer_map.reset_grid()
+print(answer_map.access_with_removal())
