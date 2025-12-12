d35811d6695597c00f99cb7553a0d1eb4a9e159f
Kathryn <137423893+kathrynpotten@users.noreply.github.com>
2025-12-12 11:18:45 +0000

    8-2 Playground

--- scripts/08_Playground.py
+++ scripts/08_Playground.py
@@ -1,8 +1,9 @@
 ## Day 8: Playground
 
 import math
-from itertools import product
+from itertools import combinations_with_replacement, product
 from collections import defaultdict
+import time
 
 # Part 1
 
@@ -51,8 +52,8 @@ class SetOfJunctions:
     def __init__(self, junctions=[], debug=False):
         self.junctions = [JunctionBox(junction) for junction in junctions]
         self.size = len(self.junctions)
-        self.distances = self.find_distances()
-        self.circuits = []
+        self.pairs = self.find_distances()
+        self.circuits = SetOfCircuits(Circuit)
         self.debug = debug
 
     def __str__(self):
@@ -62,119 +63,143 @@ class SetOfJunctions:
         return sum([(x - y) ** 2 for x, y in zip(junction1.coords, junciton2.coords)])
 
     def find_distances(self):
-        self.distances = defaultdict()
-        for junction1, junction2 in product(self.junctions, self.junctions):
+        distances = defaultdict()
+        for junction1, junction2 in combinations_with_replacement(self.junctions, 2):
             if junction1 != junction2:
-                self.distances[self.calculate_distance(junction1, junction2)] = (
+                distances[self.calculate_distance(junction1, junction2)] = (
                     junction1,
                     junction2,
                 )
-        return dict(sorted(self.distances.items()))
+        return [pair for _, pair in dict(sorted(distances.items())).items()]
 
     def find_connections(self, num_connections):
-        for dist, pair in list(self.distances.items())[:num_connections]:
+        connected = defaultdict(set)
+        count = 0
+        for pair in self.pairs[:num_connections]:
             if self.debug:
                 print(f"{pair=}")
             junction1, junction2 = pair
-            added = False
-            for circuit in self.circuits:
-                if not added:
-                    if circuit.is_in_circuit(junction1) or circuit.is_in_circuit(
-                        junction2
+            if connected[str(junction1)]:
+                circuit = list(connected[str(junction1)])[0]
+                self.circuits[circuit].add_junctions([junction2])
+                connected[str(junction2)].add(circuit)
+                if len(connected[str(junction2)]) > 1:
+                    first_circuit, second_circuit = list(connected[str(junction2)])
+                    if len(self.circuits[first_circuit]) >= len(
+                        self.circuits[second_circuit]
                     ):
-                        circuit.add_junctions(list(pair))
-                        added = True
-                        containing_circuit = circuit
-                else:
-                    if circuit.is_in_circuit(junction1) or circuit.is_in_circuit(
-                        junction2
-                    ):
-                        circuit.merge_circuits(containing_circuit)
-                        self.circuits.remove(containing_circuit)
-            if not added:
-                self.circuits.append(Circuit(list(pair)))
+                        self.circuits[first_circuit].merge_circuits(
+                            self.circuits[second_circuit]
+                        )
+                        del self.circuits[second_circuit]
+                        for junction in self.circuits[first_circuit]:
+                            connected[str(junction)] = {first_circuit}
+                    else:
+                        self.circuits[second_circuit].merge_circuits(
+                            self.circuits[first_circuit]
+                        )
+                        del self.circuits[first_circuit]
+                        for junction in self.circuits[second_circuit]:
+                            connected[str(junction)] = {second_circuit}
+            elif connected[str(junction2)]:
+                circuit = list(connected[str(junction2)])[0]
+                self.circuits[circuit].add_junctions([junction1])
+                connected[str(junction1)].add(circuit)
+            else:
+                self.circuits[count] = Circuit(count, list(pair))
+                connected[str(junction1)].add(count)
+                connected[str(junction2)].add(count)
+            count += 1
             if self.debug:
-                print([str(circuit) for circuit in self.circuits])
-        return sorted([circuit.size for circuit in self.circuits])
+                print([str(circuit) for circuit in self.circuits.values()])
+        return sorted([len(circuit) for circuit in self.circuits.values()])
 
     def largest_multiplied(self, num_connections):
         sizes = self.find_connections(num_connections)[::-1]
         return math.prod(sizes[:3])
 
     def reset(self):
-        self.distances = self.find_distances()
-        self.circuits = []
+        self.circuits = SetOfCircuits(Circuit)
 
     def connect_all(self):
+        connected = defaultdict(set)
         # add first circuit
-        dist, pair = list(self.distances.items())[0]
-        self.circuits.append(Circuit(list(pair)))
+        pair = self.pairs[0]
+        self.circuits[0] = Circuit(0, list(pair))
+        junction1, junction2 = pair
+        connected[str(junction1)].add(0)
+        connected[str(junction2)].add(0)
         # add circuits until there is only one left over
         count = 1
-        while len(self.circuits) > 1 or self.circuits[0].size < self.size - 1:
-            dist, pair = list(self.distances.items())[count]
+        while (
+            len(self.circuits.items()) > 1
+            or len(list(self.circuits.values())[0]) < self.size - 1
+        ):
+            pair = self.pairs[count]
             junction1, junction2 = pair
-            added = False
-            for circuit in self.circuits:
-                if not added:
-                    if circuit.is_in_circuit(junction1):
-                        circuit.add_junctions([junction2])
-                        added = True
-                        containing_circuit = circuit
-
-                    elif circuit.is_in_circuit(junction2):
-                        circuit.add_junctions([junction1])
-                        added = True
-                        containing_circuit = circuit
-
-                else:
-                    if circuit.is_in_circuit(junction1) or circuit.is_in_circuit(
-                        junction2
+            if connected[str(junction1)]:
+                circuit = list(connected[str(junction1)])[0]
+                self.circuits[circuit].add_junctions([junction2])
+                connected[str(junction2)].add(circuit)
+                if len(connected[str(junction2)]) > 1:
+                    first_circuit, second_circuit = list(connected[str(junction2)])
+                    if len(self.circuits[first_circuit]) >= len(
+                        self.circuits[second_circuit]
                     ):
-                        circuit.merge_circuits(containing_circuit)
-                        self.circuits.remove(containing_circuit)
-            if not added:
-                self.circuits.append(Circuit(list(pair)))
+                        self.circuits[first_circuit].merge_circuits(
+                            self.circuits[second_circuit]
+                        )
+                        del self.circuits[second_circuit]
+                        for junction in self.circuits[first_circuit]:
+                            connected[str(junction)] = {first_circuit}
+                    else:
+                        self.circuits[second_circuit].merge_circuits(
+                            self.circuits[first_circuit]
+                        )
+                        del self.circuits[first_circuit]
+                        for junction in self.circuits[second_circuit]:
+                            connected[str(junction)] = {second_circuit}
+            elif connected[str(junction2)]:
+                circuit = list(connected[str(junction2)])[0]
+                self.circuits[circuit].add_junctions([junction1])
+                connected[str(junction1)].add(circuit)
+            else:
+                self.circuits[count] = Circuit(count, list(pair))
+                connected[str(junction1)].add(count)
+                connected[str(junction2)].add(count)
             count += 1
         for junction in self.junctions:
-            if not self.circuits[0].is_in_circuit(junction):
-                distances = sorted(
-                    [
-                        (dist, pair)
-                        for dist, pair in self.distances.items()
-                        if junction in pair
-                    ]
-                )
-                min_dist, pair = distances[0]
+            if junction not in list(self.circuits.values())[0]:
+                pair = [pair for pair in self.pairs[count:] if junction in pair][0]
         junction1, junction2 = pair
         if self.debug:
             print(pair)
         return junction1.x * junction2.x
 
 
-class Circuit:
+class Circuit(list):
 
-    def __init__(self, junctions=[]):
-        self.junctions = junctions
-        self.size = len(self.junctions)
+    def __init__(self, name, junctions=[]):
+        super().__init__(junctions)
+        self.name = name
 
     def __str__(self):
-        return " ".join([str(junction) for junction in self.junctions])
+        return " ".join([str(junction) for junction in self])
 
     def add_junctions(self, junctions):
         for junction in junctions:
-            if junction not in self.junctions:
-                self.junctions.append(junction)
-        self.size = len(self.junctions)
-        return self.junctions
-
-    def is_in_circuit(self, junction):
-        if junction in self.junctions:
-            return True
-        return False
+            if junction not in self:
+                self.append(junction)
+        return self
 
     def merge_circuits(self, circuit):
-        self.add_junctions(circuit.junctions)
+        self.add_junctions(circuit)
+
+
+class SetOfCircuits(defaultdict):
+
+    def __init__(self, default_factory=None):
+        super().__init__(default_factory)
 
 
 test_set_2 = SetOfJunctions(test_data.split("\n"))
@@ -187,9 +212,9 @@ assert test_set_2.largest_multiplied(10) == 40
 with open("../input_data/08_Playground.txt", "r", encoding="utf-8") as file:
     input_data = file.read().strip()
 
+
 answer_set = SetOfJunctions(input_data.split("\n"))
 print(answer_set.largest_multiplied(1000))
-
 
 # Part 2
 
