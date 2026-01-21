5e27f6558ae9d31a5b06ec093e5af9a69da7afc9
Kathryn <137423893+kathrynpotten@users.noreply.github.com>
2026-01-21 10:43:00 +0000

    11-2 Reactor

--- scripts/11_Reactor.py
+++ scripts/11_Reactor.py
@@ -78,30 +78,116 @@ print(answer_1)
 # Part 2
 
 
-def out_path_svr(devices, key="svr", out=0, dac_flag=False, fft_flag=False):
-    starters = devices[key]
+def out_path_svr(
+    devices,
+    in_key="svr",
+    out=0,
+    dac_flag=False,
+    fft_flag=False,
+    recursion_depth=0,
+    level_dict=None,
+):
+    if level_dict is None:
+        level_dict = {}
+
+    starters = devices[in_key]
+    if len(starters) > 1:
+        for starter in starters:
+            if starter not in level_dict:
+                level_dict[starter] = recursion_depth
     for starter in starters:
-        print(f"{starter=}")
+        if starter in level_dict:
+            recursion_depth = level_dict[starter]
+            level_dict = {
+                key: val for key, val in level_dict.items() if val <= recursion_depth
+            }
+            del level_dict[starter]
+        if dac_flag and recursion_depth < dac_flag:
+            dac_flag = False
+        if fft_flag and recursion_depth < fft_flag:
+            fft_flag = False
+
         if starter == "dac":
-            dac_flag = True
+            dac_flag = recursion_depth
         elif starter == "fft":
-            fft_flag = True
+            fft_flag = recursion_depth
+
         next_devices = devices[starter]
         if "out" in next_devices:
             if dac_flag and fft_flag:
                 out += 1
-                print("out", True)
-            print("out")
-            # only reset if returning to server - need to keep track of "layers"
-            dac_flag = False
-            fft_flag = False
-
         else:
-            out, dac_flag, fft_flag = out_path_svr(
-                devices, key=starter, out=out, dac_flag=dac_flag, fft_flag=fft_flag
+            recursion_depth += 1
+            out, dac_flag, fft_flag, recursion_depth, level_dict = out_path_svr(
+                devices,
+                in_key=starter,
+                out=out,
+                dac_flag=dac_flag,
+                fft_flag=fft_flag,
+                recursion_depth=recursion_depth,
+                level_dict=level_dict,
             )
-    return out, dac_flag, fft_flag
+
+    return out, dac_flag, fft_flag, recursion_depth, level_dict
 
 
 test_devices2 = parse_data(test_data2)
-print(out_path_svr(test_devices2))
+assert out_path_svr(test_devices2)[0] == 2
+
+
+def x_to_y_path(
+    devices,
+    in_key,
+    out_key,
+    out=0,
+):
+
+    starters = devices[in_key]
+    for starter in starters:
+        next_devices = devices[starter]
+        if out_key in next_devices:
+            out += 1
+        elif "out" in next_devices:
+            continue
+        else:
+            out = x_to_y_path(
+                devices,
+                in_key=starter,
+                out_key=out_key,
+                out=out,
+            )
+    return out
+
+
+# print(x_to_y_path(test_devices2, "dac", "fft"))
+# print(x_to_y_path(test_devices2, "fft", "dac"))
+
+
+def svr_to_out_path(
+    devices,
+):
+    svr_fft_paths = 0
+    fft_out_paths = 0
+    svr_dac_paths = 0
+    dac_out_paths = 0
+
+    dac_fft_paths = x_to_y_path(devices, "dac", "fft")
+    fft_dac_paths = x_to_y_path(devices, "fft", "dac")
+
+    if dac_fft_paths:
+        svr_dac_paths = x_to_y_path(devices, "svr", "dac")
+        fft_out_paths = x_to_y_path(devices, "fft", "out")
+    if fft_dac_paths:
+        svr_fft_paths = x_to_y_path(devices, "svr", "fft")
+        dac_out_paths = x_to_y_path(devices, "dac", "out")
+
+    return (
+        svr_dac_paths * dac_fft_paths * fft_out_paths
+        + svr_fft_paths * fft_dac_paths * dac_out_paths
+    )
+
+
+assert svr_to_out_path(test_devices2) == 2
+
+answer_2 = svr_to_out_path(answer_devices)
+print(answer_2)
